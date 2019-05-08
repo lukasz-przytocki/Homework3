47c2a14d2d3f7ee6183aafae0d4c159b7b81a32c
lukasz.przytocki <lukasz.przytoci@aptitudesoftware.com>
2019-05-08 11:18:49 +0200

    Added custom constructor. Adjusted main class.

--- src/ClockEx1.java
+++ src/ClockEx1.java
@@ -3,6 +3,12 @@ public class ClockEx1 {
     boolean isDigital;
     String color;
 
+    ClockEx1(String br, boolean isD, String clr){
+        brand = br;
+        isDigital = isD;
+        color = clr;
+    }
+
     void printInfo (){
         System.out.printf("The clock name is %s, is digital %b and its color is %s. \n", brand, isDigital,color);
     }
--- src/ClockEx1Main.java
+++ src/ClockEx1Main.java
@@ -1,15 +1,15 @@
 public class ClockEx1Main {
     public static void main(String[] args) {
-        ClockEx1 clock1 = new ClockEx1();
-        ClockEx1 clock2 = new ClockEx1();
+        ClockEx1 clock1 = new ClockEx1("Sony", true, "grey");
+        ClockEx1 clock2 = new ClockEx1("Jvd", false, "black");
 
-        clock1.brand = "Sony";
-        clock1.isDigital = true;
-        clock1.color = "grey";
-
-        clock2.brand = "JVD";
-        clock2.isDigital = false;
-        clock2.color = "balck";
+//        clock1.brand = "Sony";
+//        clock1.isDigital = true;
+//        clock1.color = "grey";
+//
+//        clock2.brand = "JVD";
+//        clock2.isDigital = false;
+//        clock2.color = "black";
 
         clock1.printInfo();
         clock2.printInfo();
